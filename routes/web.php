01916c09c7523ac6e012564aaac3862adeaff849
Kamgue fokoua blondin <fokouak@gmail.com>
2020-04-10 19:47:13 +0200

    Add api

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Resources\Estimator as EstimatorResource;
-use App\Data;
-use App\Region;
-use App\Impact;
-use App\SevereImpact;
-use App\Http\Controllers\EstimatorController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,19 +17,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/challenge1','EstimatorController@covid19ImpactEstimator')->name('challenge1');
-
-
-Route::get('/api/v1/on-covid-19/', function () {
-	 $data=new Data;
-	 $data->region=new Region;
-	 $data->region->name="Africa";
-     $data->region->avgAge=19.7;
-     $data->region->avgDailyIncomeInUSD=5;
-     $data->region->avgDailyIncomePopulation=0.71;
-	 $data->periodType= "days";
-     $data->timeToElapse= 58;
-     $data->reportedCases= 674;
-     $data->population= 66622705;
-     $data->totalHospitalBeds=1380614;
-    return new EstimatorResource($data);
-});
