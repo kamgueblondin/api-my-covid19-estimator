89602a4c275d591bcb6e13dc940e6deb57e7eb35
kamgueblondin <kamgueblondin@gmail.com>
2020-04-11 10:33:44 +0200

    mise en ligne

--- app/Http/Controllers/Api/EstimatorController.php
+++ app/Http/Controllers/Api/EstimatorController.php
@@ -117,7 +117,7 @@ class EstimatorController extends Controller
      */
     public function store(Request $request)
     {
-        return($request->all());
+        return ($this->covid19ImpactEstimator($request->all()));
     }
 
 }
