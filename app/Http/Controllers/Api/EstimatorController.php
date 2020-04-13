04f837eaf4b1007052a242843b4ebf0e4b724cff
kamgueblondin <kamgueblondin@gmail.com>
2020-04-13 02:47:29 +0200

    update

--- app/Http/Controllers/Api/EstimatorController.php
+++ app/Http/Controllers/Api/EstimatorController.php
@@ -234,7 +234,7 @@ class EstimatorController extends Controller
 		$time_end = microtime(true);
         $execution_time = ($time_end - $time_start)*60;
         $log->timestamp=(int)$time_start;
-        $log->path="on-covid-19/xml";
+        $log->path="on-covid-19/logs";
         $log->second=number_format((float) $execution_time, 2);
         $log->save();
         $logs=Log::all();
